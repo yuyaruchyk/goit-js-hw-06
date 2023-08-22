0d6f97a655b095efbc551b3c79b2cab54aff7313
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-08-22 15:22:53 +0200

    upd
    upd

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -2,4 +2,19 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+const body = document.querySelector('.body');
+const btn = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+const bodyElement = document.querySelector('.widget');
+
+btn.addEventListener("click", () => {
+
+  const newColor = getRandomHexColor();
+ 
+  bodyElement.style.backgroundColor = newColor;
+  return color.textContent = newColor;
+});
+
+
